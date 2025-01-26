d9eb0b5a7a475fc6b14f8e20af75fb9eaa8abc85
Lucas Matias <lucasmatiassilva2022@gmail.com>
2025-01-26 00:25:01 -0300

    favicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BellOutlined } from '@ant-design/icons';  // Importando o ícone de sino do antd
 import PropTypes from 'prop-types'; // Para garantir que o nome da seção seja passado como prop
+import logo from '../assets/meetspace.svg'; // Importando a logo
 
 const Header = ({ sectionName }) => {
   const handleLogout = () => {
@@ -11,9 +12,12 @@ const Header = ({ sectionName }) => {
   return (
     <header className="fixed top-0 left-0 w-full p-4 bg-gray-800 text-white z-50">
       <div className="flex items-center justify-between">
-        <h1 className="text-xl font-semibold">{sectionName}</h1>  {/* Nome da seção passado por props */}
+        <div className='flex flex-row gap-4 items-center justify-center'>
+            <img src={logo} alt="Logo" className="w-8" />
+            <h1 className="text-xl font-semibold">{sectionName}</h1>
+        </div> 
         <div className="flex items-center space-x-4">
-          <BellOutlined style={{ fontSize: '24px' }} />  {/* Ícone de sino do antd */}
+          <BellOutlined style={{ fontSize: '24px' }} /> 
           <button
             onClick={handleLogout}
             className="px-4 py-2 bg-red-600 rounded hover:bg-red-500 transition"
