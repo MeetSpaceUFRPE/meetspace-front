3c7e8b372ddaa75df63c25df5a1e243a3fb67b79
brenno-araujo25 <brennocaldeira@outlook.com>
2025-03-05 15:51:52 -0300

    feat: add today's reservations display and enhance report statistics layout

--- src/pages/app/Relatorios.jsx
+++ src/pages/app/Relatorios.jsx
@@ -4,7 +4,7 @@ import { getDailySchedule, getReservationsByPeriod, getReservationFrequency, get
 import dayjs from 'dayjs';
 import Header from '../../components/Header';
 import ButtonBack from '../../components/ButtonBack';
-import { Loading3QuartersOutlined } from "@ant-design/icons";
+import { Loading3QuartersOutlined, ClockCircleOutlined } from "@ant-design/icons";
 import { useParams } from "react-router-dom";
 
 const Relatorios = () => {
@@ -12,6 +12,7 @@ const Relatorios = () => {
     const [startDate, setStartDate] = useState(dayjs().format("YYYY-MM-DD"));
     const [endDate, setEndDate] = useState(dayjs().format("YYYY-MM-DD"));
     const [reservations, setReservations] = useState([]);
+    const [todayReservations, setTodayReservations] = useState([]);
     const [frequency, setFrequency] = useState(0);
     const [occupancy, setOccupancy] = useState(0);
     const [loading, setLoading] = useState(false);
@@ -25,7 +26,9 @@ const Relatorios = () => {
             setLoading(true);
             const { frequency } = await getReservationFrequency(id, startDate, endDate);
             const { averageOccupancy } = await getAverageOccupancy(id, startDate, endDate);
+            const todayReservations = await getDailySchedule(id, dayjs().format("YYYY-MM-DD"));
             const reservations = await getReservationsByPeriod(id, startDate, endDate);
+            setTodayReservations(todayReservations);
             setReservations(reservations);
             setFrequency(frequency);
             setOccupancy(averageOccupancy);
@@ -53,37 +56,83 @@ const Relatorios = () => {
     }
 
     return (
-        <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+        <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 py-10">
             <Header sectionName={"Cronograma da Sala"} />
+
             <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-3xl">
-            <ButtonBack path={"/salas"} style="left-0 md:left-4 my-8 z-100" />
+              <ButtonBack path={"/salas"} style="left-0 md:left-4 my-8 z-100" />
             
-            <Row gutter={[16, 16]}>
-              <Col span={24} className="text-center">
-                <h1 className="text-2xl font-bold text-[#D84040]">Cronograma Diário</h1>
-              </Col>
-              <Col span={24} className="flex justify-center">
-                De: <DatePicker defaultValue={dayjs()} onChange={(date) => setStartDate(dayjs(date).format("YYYY-MM-DD"))} />
-                Até: <DatePicker defaultValue={dayjs()} onChange={(date) => setEndDate(dayjs(date).format("YYYY-MM-DD"))} />
-              </Col>
-              <Col span={24}><Divider /></Col>
-              <Col span={12}><Tag color="blue">Frequência: {frequency}</Tag></Col>
-              <Col span={12}><Tag color="purple">Ocupação Média: {occupancy}</Tag></Col>
-              <Col span={24}><Divider /></Col>
-              <Col span={24}>
-                {reservations.length === 0 ? (
-                  <p className="text-center text-gray-500">Nenhuma reserva encontrada.</p>
+              {/* TITULO */}
+              <div className="text-center mb-6">
+                <h1 className="text-3xl font-bold text-[#D84040]">Cronograma Diário</h1>
+              </div>
+
+              {/* RESERVAS DE HOJE */}
+              <Row gutter={[16, 16]}>
+                {todayReservations.length === 0 ? (
+                  <Col span={24} className="text-center text-gray-500">
+                    Nenhuma reserva para hoje.
+                  </Col>
                 ) : (
-                  reservations.map((res, index) => (
-                    <Card key={index} className="mb-4">
-                      <div className="flex justify-between items-center">
-                        <span className="font-bold">{res.turno} ({res.data})</span>
-                      </div>
-                    </Card>
+                  todayReservations.map((res, index) => (
+                    <Col span={24} key={index} className="flex justify-center">
+                        <Card className="p-2 bg-[#D84040] text-white rounded-lg shadow-lg hover:shadow-2xl w-1/2">
+                            <div className="flex justify-between items-center">
+                                <div className="flex items-center">
+                                    <ClockCircleOutlined className="mr-2 text-lg" />
+                                    <span className="font-bold text-lg">{res.turno.toUpperCase()} ({res.data})</span>
+                                </div>
+                            </div>
+                        </Card>
+                    </Col>
                   ))
                 )}
-              </Col>
-            </Row>
+
+                <Col span={24}><Divider /></Col>
+
+                {/* RESERVAS NO PERIODO */}
+                <Col span={24} className="text-center">
+                    <h1 className="text-3xl font-bold text-[#D84040]">Reservas no Período</h1>
+                </Col>
+                
+                {/* ESTATISTICAS */}
+                <Col span={24} className="text-center text-[#D84040] font-semibold text-lg">
+                    <Tag color="blue" className="text-lg rounded-md">Reservas: {frequency}</Tag>
+                    <Tag color="purple" className="text-lg rounded-md">Ocupação Média: {occupancy.toFixed(2)}</Tag>
+                </Col>
+
+                {/* FILTRO DE DATA */}
+                <Col span={24} className="flex justify-center gap-4 my-4">
+                  <div className="flex items-center">
+                      <span className="font-semibold mr-2">De:</span>
+                      <DatePicker defaultValue={dayjs()} onChange={(date) => setStartDate(dayjs(date).format("YYYY-MM-DD"))} />
+                  </div>
+                  <div className="flex items-center">
+                      <span className="font-semibold mr-2">Até:</span>
+                      <DatePicker defaultValue={dayjs()} onChange={(date) => setEndDate(dayjs(date).format("YYYY-MM-DD"))} />
+                  </div>
+                </Col>
+
+                {/* Lista de Reservas por Período */}
+                {reservations.length === 0 ? (
+                    <Col span={24} className="text-center text-gray-500">
+                        Nenhuma reserva encontrada.
+                    </Col>
+                ) : (
+                    reservations.map((res, index) => (
+                        <Col span={24} key={index} className="flex justify-center">
+                            <Card className="p-2 bg-[#D84040] text-white rounded-lg shadow-lg hover:shadow-2xl w-1/2">
+                                <div className="flex justify-between items-center">
+                                    <div className="flex items-center">
+                                        <ClockCircleOutlined className="mr-2 text-lg" />
+                                        <span className="font-bold text-lg">{res.turno.toUpperCase()} ({res.data})</span>
+                                    </div>
+                                </div>
+                            </Card>
+                        </Col>
+                    ))
+                )}
+              </Row>
           </div>
         </div>
     );
